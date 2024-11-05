9efef1d83539754ad5d592f6fafd45400f3c23cf
BirhanuI <birhanunegash9@gmail.com>
2024-11-05 09:35:04 +0300

    fixed the bug that prevented the schedule to work

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\ExamSchedule;
 use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -9,7 +10,9 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 Schedule::call(function () {
-    \App\Models\ExamSchedule::where('date', '<', Carbon::now())->get()->each(function ($schedule) {
+    ExamSchedule::where('date', '<', Carbon::now())->get()->each(function ($schedule) {
+        $duration = optional($schedule->exam)->duration;
+        if($duration == null) return;
         $examEnd = Carbon::parse($schedule->date)->addMinutes((int) $schedule->exam->duration);
         $schedule->update(['is_active' => now()->lte($examEnd) ? true : false]);
     });
